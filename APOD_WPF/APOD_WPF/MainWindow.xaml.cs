4897665aac102961e4820686b2ee385a30754340
Z3Programming <z3programming@gmail.com>
2018-12-28 16:13:48 -0500

    Set the initial controls for the MonthCalendar control

--- APOD_WPF/APOD_WPF/MainWindow.xaml.cs
+++ APOD_WPF/APOD_WPF/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace APOD_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //endpoint url
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
+
+        //APOD launch date
+        DateTime launchDate = new DateTime(1995,6,16);
+
+        //A count of images downloaded today
+        int imageCountToday;
+
+        //Flag to ignore a strange double-event in WPF
+        bool ignoreDoubleEvent = false;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Set the maximum date to today, and the minimum date to the date APOD was launched
+            MonthCalendar.DisplayDateEnd = DateTime.Today;
+            MonthCalendar.DisplayDateStart = launchDate;
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LimitRangeCheckBox.IsChecked = false;
+
+            //set the calender APOD launch date
+            //will fire double event, the first will be ignored
+            ignoreDoubleEvent = true;
+            MonthCalendar.SelectedDate = launchDate;
+        }
+
+        private void MonthCalendar_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
